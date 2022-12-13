76b6a5c46a7446dbe47924bc7ce1792e0f0f508b
noah harris <noahharris.000@gmail.com>
2022-12-13 10:56:45 -0600

    Bug Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store/public_api';
+import { StoreModule } from '@ngrx/store';
 
 // Imports from other files 
 import { AppRoutingModule } from './app-routing.module';
